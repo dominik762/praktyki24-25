47201b8ed249ff798dbf3d0a4bb15ace2f340b1f
dominik762 <dominik.ostrowski@easyripok.pl>
2024-09-09 13:37:36 +0200

    poprawa index.php

--- public/index.php
+++ public/index.php
@@ -9,7 +9,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $kernel = Kernel::getInstance();
 
 $user1 = User::find(1);
-$user1 = User::find(1);
-$user1 = User::find(1);
-$user1 = User::find(1);
 if (isset($user1)) echo $user1->getName();
