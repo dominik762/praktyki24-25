cd33c4cc65f63289e51c617f374d5da9d5b4978d
dominik762 <dominik.ostrowski@easyripok.pl>
2024-09-13 14:39:31 +0200

    poprawki

---
+++ src/Views/indexView/header.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang='pl'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='stylesheet' href='/praktyki24-25/public/styles/styles-index.css'>
+    <title>@yield('title', 'Default Title')</title>
+</head>
+<body>
