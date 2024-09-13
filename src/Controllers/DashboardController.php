9961f54f0b39e0cac27b1db168b02882637b2263
dominik762 <dominik.ostrowski@easyripok.pl>
2024-09-13 10:03:51 +0200

    uzycie jenssegers/blade

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controllers;
 
+use App\View;
+
 class DashboardController
 {
     public function show()
     {
-        echo "to jest dashboard<br>";
+        echo View::render('dashboard', [
+            'title' => 'Dashboard',
+        ]);
     }
 
 }
---
+++ src/View.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Jenssegers\Blade\Blade;
+class View
+{
+    private static $instance = null;
+
+    public static function getInstance(): Blade
+    {
+        if (self::$instance === null) {
+            self::$instance = new Blade(__DIR__ . '/Views', __DIR__ . '/../storage/cache');
+        }
+
+        return self::$instance;
+    }
+    public static function render($view, $data = []):string
+    {
+        return self::getInstance()->render($view, $data);
+    }
+
+}
