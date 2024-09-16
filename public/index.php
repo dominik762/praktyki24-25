e065d5dc635dd9fcec6b54e7929d32e025419a1f
dominik762 <dominik.ostrowski@easyripok.pl>
2024-09-16 10:49:01 +0200

    poprawki

--- public/index.php
+++ public/index.php
@@ -4,9 +4,6 @@ use App\Kernel;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../secured/');
-$dotenv->load();
-
 $kernel = Kernel::getInstance();
 
 $kernel->run();
