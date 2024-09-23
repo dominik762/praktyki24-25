f0d5ab4e34b04bd19429b4207fee8833e4f7049b
dominik762 <dominik.ostrowski@easyripok.pl>
2024-09-23 09:47:05 +0200

    Error Modes v3 - working version

--- src/Middleware/EnsureUserIsLoggedInMiddleware.php
+++ src/Middleware/EnsureUserIsLoggedInMiddleware.php
@@ -16,19 +16,8 @@ class EnsureUserIsLoggedInMiddleware implements MiddlewareInterface
      */
     public function handle(): void
     {
-        $availableRoutes = new Urls();
-        $availableRoutes->loadUrls();
-
-        $routeKey = ($_GET['controller'] . '.' . $_GET['do']);
-
-        if (array_key_exists($routeKey, (array)$availableRoutes)) {
-            $route = $availableRoutes[$routeKey];
-
-            if (in_array('true', $route['middleware'])) {
-                if (empty($_SESSION['userId'])) {
-                    Redirect::to('authuser.signIn');
-                }
-            }
+        if (empty($_SESSION['userId'])) {
+            Redirect::to('authuser.signIn');
         }
     }
 }
